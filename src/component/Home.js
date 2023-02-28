170c49b2320e5491e56bc80faba2323f36f0a419
SwaroopKhot <swaroopkhot210@gmail.com>
2023-02-28 11:59:36 +0530

    Changes

--- src/component/Home.js
+++ src/component/Home.js
@@ -6,8 +6,20 @@ import './Home.css'
 function Home() {
     return (
         <div className="home_main_container">
-            <input className="encode_btn" type="file" id="encodefile" name="encodeFile" />
-            <button>Decode</button>
+
+            <div className="home_button_div">
+
+                <div className="">
+                    <label for='encodefile' className="encode_btn_label">Encode</label>
+                    <input className="encode_btn" type="file" id="encodefile" name="encodeFile" />
+                </div>
+                
+
+                <div>
+                    <label for='decodefile' className="encode_btn_label">Decode</label>
+                    <input className="encode_btn" type="file" id="decodefile" name="encodeFile" />
+                </div>
+            </div>
         </div>
     )
 }
